706377e1482592dda3c5975ac6ce20400b9a271e
Bill <Bill@>
2018-07-17 08:37:58 +0200

    added daemonize_proc() in run_if_bootstrap.py

--- run_if_boostrap.py
+++ run_if_boostrap.py
@@ -8,10 +8,11 @@ import logging
 import subprocess
 
 def copy_jar_when_ready(source,destination,check):
+    '''copy jar when pre-condition is met'''
     logging.warn('startin process {} in master'.format(os.getpid()))
     while not os.path.isfile(check):
         logging.warn('waiting for file to exist ...')
-        time.sleep(10)
+        time.sleep(5)
     download_file(source,destination)
 
 def download_file(source,destination):
@@ -19,44 +20,38 @@ def download_file(source,destination):
     subprocess.call(['sudo','wget',source,'-P',destination])
 
 def is_master(instance_info):
+    '''check if instance is master'''
     is_master = False
     try:
         with open(instance_info) as f:
             instance_info_dict = json.load(f)
-        #get instance info
         is_master = instance_info_dict.get('isMaster')
     except IOError as e:
         print(e)
-    
     return is_master
+
+def daemonize_proc():
+    '''run a new process in the background'''
+    pid = os.fork()
+    try:
+        if pid > 0:
+            sys.exit(0)
+    except OSError, e:
+        logging.error("Failed to Demonize: %d, %s\n" % (e.errno,e.strerror))
+        sys.exit(1)
 
 
 if __name__=="__main__":
-    #init vars jar_source = sys.argv[1], jar_destination = sys.argv[2] ,
-    #check = sys.argv[3]
     jar_source = 'http://central.maven.org/maven2/com/google/guava/guava/25.1-jre/guava-25.1-jre.jar'
     jar_destination = '/usr/lib/spark/jars/'
     instance_info = "/mnt/var/lib/info/instance.json"
     check = '/var/run/spark/spark-history-server.pid'
     
     if is_master(instance_info):
-        pid = os.fork()
-        try:
-            if pid > 0:
-                sys.exit(0)
-        except OSError, e:
-            logging.error("Failed to Demonize: %d, %s\n" % (e.errno,e.strerror))
-            sys.exit(1)
-        
+        daemonize_proc()
         os.setsid()
-        pid = os.fork()
-        try:
-            if pid > 0:
-                sys.exit(0)
-        except OSError, e:
-            logging.error("Failed to Demonize: %d, %s\n" % (e.errno,e.strerror))
-            sys.exit(1)
+        daemonize_proc()
         copy_jar_when_ready(jar_source,jar_destination,check)
     else:
-        logging.warn('instance is not mastser, exiting ..')
+        logging.warn('instance is not master, exiting ..')
         sys.exit(0)
